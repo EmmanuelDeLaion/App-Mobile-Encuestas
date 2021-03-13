ba2c061741a4914c95651b4787772e585a26f21e
Mono <Mono@DESKTOP-BR5IUL7>
2021-03-13 17:44:18 -0600

    pantalla login terminada

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
  
 const colorPrimary = Color.fromRGBO(39, 170, 242, 1);
 const colorPrimaryLigth = Color.fromRGBO(39, 170, 242, 0.5);
+const colorPrimaryLigth2 = Color.fromRGBO(39, 170, 242, 0.2);
