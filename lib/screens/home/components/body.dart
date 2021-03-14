cb9f01be395ad379ac2dc4eb4004f7dc20d97f7e
Mono <Mono@DESKTOP-BR5IUL7>
2021-03-13 19:45:47 -0600

    pantalla home search

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -5,66 +5,96 @@ class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Column(
-      children: <Widget>[
-        Container(
-          height: size.height * 0.2,
-          color: Colors.white,
-          child: Stack(
-            children: <Widget>[
-              Container(
-                height: size.height * 0.2 - 27,
-                decoration: BoxDecoration(
-                    color: colorPrimary,
-                    borderRadius: BorderRadius.only(
-                      bottomRight: Radius.circular(36),
-                      bottomLeft: Radius.circular(36),
-                    )),
-              ),
-              Positioned(
-                bottom: 0,
-                left: 0,
-                right: 0,
-                child: Container(
-                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                  margin: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
-                  height: 54,
-                  decoration: BoxDecoration(
+    return SingleChildScrollView(
+        child: Column(children: <Widget>[HeaderSearchTitle(size: size)]));
+  }
+}
+
+class HeaderSearchTitle extends StatelessWidget {
+  const HeaderSearchTitle({
+    Key key,
+    @required this.size,
+  }) : super(key: key);
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 50 * 2.5),
+      height: size.height * 0.2,
+      color: Colors.white,
+      child: Stack(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(left: 20, bottom: 40),
+            height: size.height * 0.2 - 27,
+            decoration: BoxDecoration(
+                color: colorPrimary,
+                borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(36),
+                  bottomLeft: Radius.circular(36),
+                )),
+            child: Row(
+              children: <Widget>[
+                Text(
+                  "Seleccione una encuesta",
+                  style: TextStyle(
+                      fontSize: 20,
                       color: Colors.white,
-                      borderRadius: BorderRadius.circular(20),
-                      boxShadow: [
-                        BoxShadow(
-                            offset: Offset(0, 10),
-                            blurRadius: 50,
-                            color: Colors.black.withOpacity(0.23))
-                      ]),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: TextField(
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            hintText: "Buscar",
-                            hintStyle: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black.withOpacity(0.23)),
-                            focusedBorder: InputBorder.none,
-                          ),
-                        ),
-                      ),
-                      Image(
-                        image: AssetImage(
-                          "assets/icons/iconobuscar.png"
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
+                      fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
           ),
-        ),
-      ],
+          Positioned(
+            bottom: 10,
+            left: 0,
+            right: 0,
+            child: Container(
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+              margin: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+              height: 54,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                        offset: Offset(20, 0),
+                        blurRadius: 10,
+                        color: Colors.black.withOpacity(0.23))
+                  ]),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Buscar",
+                        hintStyle: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black.withOpacity(0.23)),
+                        focusedBorder: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  Image(
+                    image: AssetImage("assets/icons/iconobuscar.png"),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            top: 10,
+            right: -20,
+            child: Image.asset(
+              "assets/images/puntos.png",
+              width: size.width * 0.2,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
